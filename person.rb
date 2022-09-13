3ec65b19cf5812a7798a1eaad70b1d3dcaaf4935
mr-abdellah <belkaid.abdulah@gmail.com>
2022-09-13 05:19:18 +0100

    Add decorator to school classes

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..99_999)
     @name = name
     @age = age
@@ -13,9 +16,17 @@ class Person
     return true if @age >= 18 || parent_permission == true
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     return true if @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+person.can_use_services?
